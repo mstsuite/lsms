95aecefbf336a8e4517d201245496b2dab56c4b4
Markus Eisenbach <eisenbachm@ornl.gov>
2020-07-16 13:26:57 -0400

    new potentials and electron configurations

---
+++ Potentials/elements.lua
@@ -0,0 +1,110 @@
+atom_type["Va"] = {Z= 0, Zc= 0, Zs=  0, Zv= 0, rad=2}
+atom_type["H"]  = {Z= 1, Zc= 0, Zs=  0, Zv= 1, rad=2}
+atom_type["He"] = {Z= 2, Zc= 2, Zs=  0, Zv= 0, rad=2}
+--
+atom_type["Li"] = {Z= 3, Zc= 2, Zs=  0, Zv= 1, rad=2}
+atom_type["Be"] = {Z= 4, Zc= 2, Zs=  0, Zv= 2, rad=2}
+atom_type["B"]  = {Z= 5, Zc= 2, Zs=  0, Zv= 3, rad=2}
+atom_type["C"]  = {Z= 6, Zc= 2, Zs=  0, Zv= 4, rad=2}
+atom_type["N"]  = {Z= 7, Zc= 2, Zs=  0, Zv= 5, rad=2}
+atom_type["O"]  = {Z= 8, Zc= 2, Zs=  0, Zv= 6, rad=2}
+atom_type["F"]  = {Z= 9, Zc= 2, Zs=  0, Zv= 7, rad=2}
+atom_type["Ne"] = {Z=10, Zc= 2, Zs=  8, Zv= 0, rad=2}
+atom_type["Na"] = {Z=11, Zc= 2, Zs=  8, Zv= 1, rad=2}
+atom_type["Mg"] = {Z=12, Zc= 2, Zs=  8, Zv= 2, rad=2}
+atom_type["Al"] = {Z=13, Zc= 2, Zs=  8, Zv= 3, rad=2}
+atom_type["Si"] = {Z=14, Zc= 2, Zs=  8, Zv= 4, rad=2}
+atom_type["P"]  = {Z=15, Zc= 2, Zs=  8, Zv= 5, rad=2}
+atom_type["S"]  = {Z=16, Zc= 2, Zs=  8, Zv= 6, rad=2}
+atom_type["Cl"] = {Z=17, Zc= 2, Zs=  8, Zv= 7, rad=2}
+atom_type["Ar"] = {Z=18, Zc=10, Zs=  8, Zv= 0, rad=2}
+--
+atom_type["K"]  = {Z=19, Zc=10, Zs=  8, Zv= 1, rad=2}
+atom_type["Ca"] = {Z=20, Zc=10, Zs=  8, Zv= 2, rad=2}
+atom_type["Sc"] = {Z=21, Zc=10, Zs=  8, Zv= 3, rad=2}
+atom_type["Ti"] = {Z=22, Zc=10, Zs=  8, Zv= 4, rad=2}
+atom_type["V"]  = {Z=23, Zc=10, Zs=  8, Zv= 5, rad=2}
+atom_type["Cr"] = {Z=24, Zc=10, Zs=  8, Zv= 6, rad=2}
+atom_type["Mn"] = {Z=25, Zc=10, Zs=  8, Zv= 7, rad=2}
+atom_type["Fe"] = {Z=26, Zc=10, Zs=  8, Zv= 8, rad=2}
+atom_type["Co"] = {Z=27, Zc=10, Zs=  8, Zv= 9, rad=2}
+atom_type["Ni"] = {Z=28, Zc=10, Zs=  8, Zv=10, rad=2}
+atom_type["Cu"] = {Z=29, Zc=10, Zs=  8, Zv=11, rad=2}
+atom_type["Zn"] = {Z=30, Zc=10, Zs=  8, Zv=12, rad=2}
+atom_type["Ga"] = {Z=31, Zc=10, Zs=  8, Zv=13, rad=2}
+atom_type["Ge"] = {Z=32, Zc=10, Zs=  8, Zv=14, rad=2}
+atom_type["As"] = {Z=33, Zc=10, Zs=  8, Zv=15, rad=2}
+atom_type["Se"] = {Z=34, Zc=18, Zs= 10, Zv= 6, rad=2}
+atom_type["Br"] = {Z=35, Zc=10, Zs=  8, Zv=17, rad=2}
+atom_type["Kr"] = {Z=36, Zc=28, Zs=  8, Zv= 0, rad=2}
+--
+atom_type["Rb"] = {Z=37, Zc=28, Zs=  8, Zv= 1, rad=2}
+atom_type["Sr"] = {Z=38, Zc=28, Zs=  8, Zv= 2, rad=2}
+atom_type["Y"]  = {Z=39, Zc=28, Zs=  8, Zv= 3, rad=2}
+atom_type["Zr"] = {Z=40, Zc=28, Zs=  8, Zv= 4, rad=2}
+atom_type["Nb"] = {Z=41, Zc=28, Zs=  8, Zv= 5, rad=2}
+atom_type["Mo"] = {Z=42, Zc=28, Zs=  8, Zv= 6, rad=2}
+atom_type["Tc"] = {Z=43, Zc=28, Zs=  8, Zv= 7, rad=2}
+atom_type["Ru"] = {Z=44, Zc=28, Zs=  8, Zv= 8, rad=2}
+atom_type["Rh"] = {Z=45, Zc=28, Zs=  8, Zv= 9, rad=2}
+atom_type["Pd"] = {Z=46, Zc=28, Zs=  8, Zv=10, rad=2}
+atom_type["Ag"] = {Z=47, Zc=28, Zs=  8, Zv=11, rad=2}
+atom_type["Cd"] = {Z=48, Zc=28, Zs=  8, Zv=12, rad=2}
+atom_type["In"] = {Z=49, Zc=28, Zs= 18, Zv= 3, rad=2}
+atom_type["Sn"] = {Z=50, Zc=28, Zs= 18, Zv= 4, rad=2}
+atom_type["Sb"] = {Z=51, Zc=28, Zs= 18, Zv= 5, rad=2}
+atom_type["Te"] = {Z=52, Zc=28, Zs= 18, Zv= 6, rad=2}
+atom_type["I"]  = {Z=53, Zc=28, Zs= 18, Zv= 7, rad=2}
+atom_type["Xe"] = {Z=54, Zc=46, Zs=  8, Zv= 0, rad=2}
+--
+atom_type["Cs"] = {Z=55, Zc=46, Zs=  8, Zv= 1, rad=2}
+atom_type["Ba"] = {Z=56, Zc=46, Zs=  8, Zv= 2, rad=2}
+atom_type["La"] = {Z=57, Zc=36, Zs= 18, Zv= 3, rad=2}
+atom_type["Ce"] = {Z=58, Zc=36, Zs= 18, Zv= 4, rad=2}
+atom_type["Pr"] = {Z=59, Zc=46, Zs= 11, Zv= 2, rad=2}
+atom_type["Nd"] = {Z=60, Zc=46, Zs= 12, Zv= 2, rad=2}
+atom_type["Pm"] = {Z=61, Zc=46, Zs= 13, Zv= 2, rad=2}
+atom_type["Sm"] = {Z=62, Zc=36, Zs= 12, Zv=14, rad=2}
+atom_type["Eu"] = {Z=63, Zc=46, Zs= 15, Zv= 2, rad=2}
+atom_type["Gd"] = {Z=64, Zc=36, Zs= 10, Zv=18, rad=2}
+atom_type["Tb"] = {Z=65, Zc=46, Zs= 17, Zv= 2, rad=2}
+atom_type["Dy"] = {Z=66, Zc=46, Zs= 18, Zv= 2, rad=2}
+atom_type["Ho"] = {Z=67, Zc=46, Zs= 19, Zv= 2, rad=2}
+atom_type["Er"] = {Z=68, Zc=46, Zs= 20, Zv= 2, rad=2}
+atom_type["Tm"] = {Z=69, Zc=46, Zs= 21, Zv= 2, rad=2}
+atom_type["Yb"] = {Z=70, Zc=46, Zs= 22, Zv= 2, rad=2}
+atom_type["Lu"] = {Z=71, Zc=46, Zs= 22, Zv= 3, rad=2}
+atom_type["Hf"] = {Z=72, Zc=46, Zs= 22, Zv= 4, rad=2}
+atom_type["Ta"] = {Z=73, Zc=46, Zs= 22, Zv= 5, rad=2}
+atom_type["W"]  = {Z=74, Zc=46, Zs= 22, Zv= 6, rad=2}
+atom_type["Re"] = {Z=75, Zc=46, Zs= 22, Zv= 7, rad=2}
+atom_type["Os"] = {Z=76, Zc=46, Zs= 22, Zv= 8, rad=2}
+atom_type["Ir"] = {Z=77, Zc=46, Zs= 22, Zv= 9, rad=2}
+atom_type["Pt"] = {Z=78, Zc=46, Zs= 22, Zv=10, rad=2}
+atom_type["Au"] = {Z=79, Zc=46, Zs= 22, Zv=11, rad=2}
+atom_type["Hg"] = {Z=80, Zc=46, Zs= 22, Zv=12, rad=2}
+atom_type["Tl"] = {Z=81, Zc=46, Zs= 32, Zv= 3, rad=2}
+atom_type["Pb"] = {Z=82, Zc=60, Zs= 18, Zv= 4, rad=2}
+atom_type["Bi"] = {Z=83, Zc=60, Zs= 18, Zv= 5, rad=2}
+atom_type["Po"] = {Z=84, Zc=60, Zs= 18, Zv= 6, rad=2}
+atom_type["At"] = {Z=85, Zc=60, Zs= 18, Zv= 7, rad=2}
+atom_type["Rn"] = {Z=86, Zc=60, Zs= 18, Zv= 8, rad=2}
+--
+atom_type["Fr"] = {Z=87, Zc=68, Zs= 10, Zv= 9, rad=2}
+atom_type["Ra"] = {Z=88, Zc=68, Zs= 10, Zv=10, rad=2}
+atom_type["Ac"] = {Z=89, Zc=68, Zs= 10, Zv=11, rad=2}
+atom_type["Th"] = {Z=90, Zc=68, Zs= 10, Zv=12, rad=2}
+atom_type["Pa"] = {Z=91, Zc=68, Zs= 10, Zv=13, rad=2}
+atom_type["U"]  = {Z=92, Zc=68, Zs= 10, Zv=14, rad=2}
+atom_type["Np"] = {Z=93, Zc=68, Zs= 10, Zv=15, rad=2}
+atom_type["Pu"] = {Z=94, Zc=68, Zs= 10, Zv=16, rad=2}
+atom_type["Am"] = {Z=95, Zc=68, Zs= 10, Zv=17, rad=2}
+atom_type["Cm"] = {Z=96, Zc=68, Zs= 10, Zv=18, rad=2}
+atom_type["Bk"] = {Z=97, Zc=68, Zs= 10, Zv=19, rad=2}
+atom_type["Cf"] = {Z=98, Zc=68, Zs= 10, Zv=20, rad=2}
+atom_type["Es"] = {Z=99, Zc=68, Zs= 10, Zv=21, rad=2}
+atom_type["Fm"] = {Z=100, Zc=68, Zs= 10, Zv=22, rad=2}
+atom_type["Md"] = {Z=101, Zc=68, Zs= 10, Zv=23, rad=2}
+atom_type["No"] = {Z=102, Zc=68, Zs= 10, Zv=24, rad=2}
+atom_type["Lr"] = {Z=103, Zc=68, Zs= 10, Zv=25, rad=2}
+
