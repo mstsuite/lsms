ed2d03ea02b476f8388c3614d1576854cb14a6f9
Franco Moitzi <franco.moitzi@mcl.at>
2022-02-11 09:57:13 +0100

    fix(MultipoleMadelung): issue with wrongly calculated isertion order

--- src/MultipoleMadelung/calculateMultipoleMadelung.cpp
+++ src/MultipoleMadelung/calculateMultipoleMadelung.cpp
@@ -9,22 +9,15 @@
 void calculateMultiMadelungMatrices(LSMSSystemParameters &lsms,
                                     CrystalParameters &crystal,
                                     LocalTypeInfo &local) {
-  std::cout << "-------- 1.1 ----------" << std::endl;
 
   std::vector<int> gindex(local.num_local);
-
-  std::cout << "-------- 1.2 ----------" << std::endl;
 
   if (lsms.global.iprint >= 0) {
     std::printf("Multipole madelung calculations!\n");
   }
 
-  std::cout << "-------- 1.3 ----------" << std::endl;
-
   lsms::MultipoleMadelung madelung(crystal.bravais, crystal.position,
                                    crystal.num_atoms, 0, local.global_id);
-
-  std::cout << "-------- 1.4 ----------" << std::endl;
 
   for (auto j = 0; j < local.num_local; j++) {
     local.atom[j].madelungMatrix.resize(crystal.num_atoms);
