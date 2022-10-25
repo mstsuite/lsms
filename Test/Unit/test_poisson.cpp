8899c2f4cd993067f843dad2bd58ed1b925fc2fa
Franco Moitzi <franco.moitzi@mcl.at>
2022-10-25 07:48:38 +0200

    add second version of poisson

--- Test/Unit/test_poisson.cpp
+++ Test/Unit/test_poisson.cpp
@@ -149,4 +149,78 @@ TEST(IntegrationPoisson, CompareDirect) {
       << "All tests have finished";
 }
 
+TEST(IntegrationPoisson, CompareExponetialMesh) {
+  constexpr auto number_of_points = 800;
+
+  std::vector<double> radial_mesh(number_of_points, 0.0);
+  std::vector<double> radial_mesh_deriv(number_of_points, 0.0);
+  std::vector<double> function(number_of_points, 0.0);
+  std::vector<double> density(number_of_points, 0.0);
+
+  std::vector<double> integral(number_of_points, 0.0);
+  std::vector<double> integrand(number_of_points, 0.0);
+
+  std::vector<double> reference(number_of_points, 0.0);
+
+  std::vector<double> vhartreederiv(number_of_points, 0.0);
+  std::vector<double> vhartree(number_of_points, 0.0);
+
+  auto r0 = 0.00001;
+  auto rmax = 2.0;
+
+  auto h = std::log(rmax / r0) / (number_of_points - 1);
+
+  for (auto i = 0; i < number_of_points; i++) {
+    radial_mesh[i] = r0 * exp(i * h);
+  }
+
+  /*
+   * 1. Test
+   *
+   * \rho(r) = 4 * pi * r^2
+   *
+   * Analytic reference solution:
+   *
+   * V(r) = - 2/3 * pi * ( x*x - 12)
+   *
+   */
+
+  for (auto i = 0; i < number_of_points; i++) {
+    density[i] = 4 * M_PI * radial_mesh[i] * radial_mesh[i];
+    auto &r = radial_mesh[i];
+    reference[i] = -M_PI * 2.0 / 3.0 * (r * r - 12);
+  }
+
+  radial_poisson(vhartree, vhartreederiv, radial_mesh, h, density,
+                 number_of_points);
+
+  for (auto i = 0; i < number_of_points; i++) {
+    ASSERT_TRUE(relative_diff(reference[i], vhartree[i]) < 2e-11);
+  }
+
+  /*
+   * 2. Test
+   *
+   * \rho(r) = 4 * pi * r^2 * exp(-r)
+   *
+   * Analytic reference solution:
+   *
+   * V(r) = 4 * pi * (- e^(-x) * (x + 2) / x + 2/ x - 3/ e^(2))
+   *
+   */
+
+  for (auto i = 0; i < number_of_points; i++) {
+    auto &r = radial_mesh[i];
+    density[i] = 4 * M_PI * r * r * exp(-r);
+    reference[i] = 4 * M_PI * (-exp(-r) * (r + 2) / r + 2 / r - 3 / exp(2));
+  }
+
+  radial_poisson(vhartree, vhartreederiv, radial_mesh, h, density,
+                 number_of_points);
+
+  for (auto i = 0; i < number_of_points; i++) {
+    ASSERT_TRUE(relative_diff(reference[i], vhartree[i]) < 1e-7);
+  }
+}
+
 }  // namespace poisson_tests
--- src/Misc/poisson.hpp
+++ src/Misc/poisson.hpp
@@ -110,7 +110,8 @@ void radial_poisson(std::vector<T> &VHartree, std::vector<T> &VHartreeDeriv,
 
   lam = 251.0 / 720.0;
 
-  i0 = VHartree[idx] +
+  i0 =
+      VHartree[idx] +
       1.0 / 720.0 * (-19.0 * f0[0] - 106.0 * f1[0] - 264 * f2[0] + 646 * f3[0]);
   i1 = VHartreeDeriv[idx] +
        1.0 / 720.0 *
@@ -122,10 +123,8 @@ void radial_poisson(std::vector<T> &VHartree, std::vector<T> &VHartreeDeriv,
   VHartree[idx + 1] = i0 + lam * Rp[idx + 1] * i1 / delta;
   VHartreeDeriv[idx + 1] = i1 / delta;
 
-
   // Adams-Moulton 6
   for (idx = 4; idx < end - 1; ++idx) {
-
     rad_func(R[idx], Rp[idx], density[idx], VHartreeDeriv[idx], f4);
 
     lam = 475.0 / 1440.0;
@@ -156,9 +155,127 @@ void radial_poisson(std::vector<T> &VHartree, std::vector<T> &VHartreeDeriv,
 
     f3[0] = f4[0];
     f3[1] = f4[1];
-
   }
 }
+
+template <typename T>
+void radial_poisson(std::vector<T> &VHartree, std::vector<T> &VHartreeDeriv,
+                    const std::vector<T> &R, const T h,
+                    const std::vector<T> &density, int end) {
+  int idx;
+  T yp[2];
+  T f0[2];
+  T f1[2];
+  T f2[2];
+  T f3[2];
+  T f4[2];
+  T delta;
+  T lam;
+  T i0;
+  T i1;
+
+  std::vector<T> density_r(end, 0.0);
+  for (idx = 0; idx < end; idx++) {
+    density_r[idx] = density[idx] / R[idx];
+  }
+
+  // Starter condition
+  VHartree[0] = radialIntegral(density_r, R, end);
+  VHartreeDeriv[0] = 0;
+
+  // PC Euler
+  rad_func(R[0], R[0] * h, density[0], VHartreeDeriv[0], f0);
+  yp[0] = VHartree[0] + f0[0];
+  yp[1] = VHartreeDeriv[0] + f0[1];
+  rad_func(R[1], R[1] * h, density[1], yp[1], f1);
+  VHartree[1] = VHartree[0] + (f0[0] + f1[0]) / 2;
+  VHartreeDeriv[1] = VHartreeDeriv[0] + (f0[1] + f1[1]) / 2;
+
+  // Adams-Moulton 3
+  idx = 1;
+
+  rad_func(R[idx], R[idx] * h, density[idx], VHartreeDeriv[idx], f1);
+  lam = 5.0 / 12.0;
+
+  i0 = VHartree[idx] + 1.0 / 12.0 * (-f0[0] + 8.0 * f1[0]);
+  i1 = VHartreeDeriv[idx] + 1.0 / 12.0 * (-f0[1] + 8.0 * f1[1]) -
+       R[idx + 1] * h * lam * density[idx + 1] / (R[idx + 1] * R[idx + 1]);
+
+  delta = 1 + lam * 2.0 * h;
+
+  VHartree[idx + 1] = i0 + lam * R[idx + 1] * h * i1 / delta;
+  VHartreeDeriv[idx + 1] = i1 / delta;
+
+  // Adams-Moulton 4
+  idx = 2;
+
+  rad_func(R[idx], R[idx] * h, density[idx], VHartreeDeriv[idx], f2);
+
+  lam = 9.0 / 24.0;
+
+  i0 = VHartree[idx] + 1.0 / 24.0 * (f0[0] - 5.0 * f1[0] + 19.0 * f2[0]);
+  i1 = VHartreeDeriv[idx] + 1.0 / 24.0 * (f0[1] - 5.0 * f1[1] + 19.0 * f2[1]) -
+       R[idx + 1] * h * lam * density[idx + 1] / (R[idx + 1] * R[idx + 1]);
+
+  delta = 1 + lam * 2.0 * h;
+
+  VHartree[idx + 1] = i0 + lam * R[idx + 1] * h * i1 / delta;
+  VHartreeDeriv[idx + 1] = i1 / delta;
+
+  // Adams-Moulton 5
+  idx = 3;
+  rad_func(R[idx], R[idx] * h, density[idx], VHartreeDeriv[idx], f3);
+
+  lam = 251.0 / 720.0;
+
+  i0 =
+      VHartree[idx] +
+      1.0 / 720.0 * (-19.0 * f0[0] - 106.0 * f1[0] - 264 * f2[0] + 646 * f3[0]);
+  i1 = VHartreeDeriv[idx] +
+       1.0 / 720.0 *
+           (-19.0 * f0[1] - 106.0 * f1[1] - 264.0 * f2[0] + 646 * f3[1]) -
+       R[idx + 1] * h * lam * density[idx + 1] / (R[idx + 1] * R[idx + 1]);
+
+  delta = 1 + lam * 2.0 * h;
+
+  VHartree[idx + 1] = i0 + lam * R[idx + 1] * h * i1 / delta;
+  VHartreeDeriv[idx + 1] = i1 / delta;
+
+  // Adams-Moulton 6
+  for (idx = 4; idx < end - 1; ++idx) {
+    rad_func(R[idx], R[idx] * h, density[idx], VHartreeDeriv[idx], f4);
+
+    lam = 475.0 / 1440.0;
+
+    i0 = VHartree[idx] + 1.0 / 1440.0 *
+                             (27.0 * f0[0] - 173.0 * f1[0] + 482.0 * f2[0] -
+                              798.0 * f3[0] + 1427.0 * f4[0]);
+
+    i1 = VHartreeDeriv[idx] +
+         1.0 / 1440.0 *
+             (27.0 * f0[1] - 173.0 * f1[1] + 482.0 * f2[1] - 798.0 * f3[1] +
+              1427.0 * f4[1]) -
+         R[idx + 1] * h * lam * density[idx + 1] / (R[idx + 1] * R[idx + 1]);
+
+    delta = 1 + lam * 2.0 * h;
+
+    VHartree[idx + 1] = i0 + lam * R[idx + 1] * h * i1 / delta;
+    VHartreeDeriv[idx + 1] = i1 / delta;
+
+    f0[0] = f1[0];
+    f0[1] = f1[1];
+
+    f1[0] = f2[0];
+    f1[1] = f2[1];
+
+    f2[0] = f3[0];
+    f2[1] = f3[1];
+
+    f3[0] = f4[0];
+    f3[1] = f4[1];
+  }
+}
+
 }  // namespace lsms
 
 #endif  // LSMS_POISSON_HPP
