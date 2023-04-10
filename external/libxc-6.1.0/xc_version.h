177d9267753e6e1e7dfbd02a40539d9720f4175e
Markus Eisenbach <eisenbachm@ornl.gov>
2023-04-10 13:15:26 -0400

    changed to libxc 6.1.0

---
+++ external/libxc-6.1.0/xc_version.h
@@ -0,0 +1,27 @@
+#if 0
+/*
+ Copyright (C) 2012 M.A.L. Marques, M. Oliveira
+
+ This Source Code Form is subject to the terms of the Mozilla Public
+ License, v. 2.0. If a copy of the MPL was not distributed with this
+ file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#endif
+
+#ifndef _XC_VERSION_H
+#define _XC_VERSION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define XC_VERSION "6.1.0"
+#define XC_MAJOR_VERSION 6
+#define XC_MINOR_VERSION 1
+#define XC_MICRO_VERSION 0
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
